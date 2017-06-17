91f98a4a5f48514885aa55f13e9396e0925cdeff
Gery Egan <gery.egan@gmail.com>
2017-06-16 22:02:54 -0700

    schema queries dummy json-server

--- schema/schema.js
+++ schema/schema.js
@@ -1,5 +1,6 @@
 const graphql = require('graphql');
-const _ = require('lodash');
+const axios = require('axios');
+
 const {
     GraphQLObjectType,
     GraphQLString,
@@ -9,15 +10,15 @@ const {
 
 const users = [
     { id: '23', firstName: 'Bill', age: 20 },
-    { id: '47', firstName: 'Samantha', age: 21}
-]
+    { id: '47', firstName: 'Samantha', age: 21 }
+];
 
 const UserType = new GraphQLObjectType({
     name: 'User',
     fields: {
         id: { type: GraphQLString },
-        fistName: { type: GraphQLString},
-        age: { type: GraphQLInt},
+        fistName: { type: GraphQLString },
+        age: { type: GraphQLInt },
     }
 });
 
@@ -28,11 +29,12 @@ const RootQuery = new GraphQLObjectType({
             type: UserType,
             args: { id: { type: GraphQLString } },
             resolve(parentValue, args) {
-                return _.find(users, { id: args.id })
+                return axios.get(`http://localhost:3000/users/${args.id}`)
+                    .then(resp => resp.data);
             }
         }
     }
-})
+});
 
 module.exports = new GraphQLSchema({
     query: RootQuery
